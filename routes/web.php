536ba558b2f2e885ae3c867ae8a7f5bf5b66396f
balfordmdiaz <balfordmdiaz@github.com>
2020-12-04 18:39:04 -0600

    Validacion de redireccion de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\MessageClient;
@@ -36,26 +37,36 @@ Route::get('/', function () {
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/home/cliente',[ClienteController::class, 'index'])->name('cliente.index');
-Route::get('/home/cliente/insertar',[ClienteController::class, 'insertar'])->name('cliente.insertar');
-Route::get('/home/cliente/{Idcliente}',[ClienteController::class, 'show'])->name('cliente.show');
-Route::get('/home/cliente/{Idcliente}/edit',[ClienteController::class, 'edit'])->name('cliente.edit');
+Route::middleware(['auth'])->group(function ()
+{
+        Route::get('/home/cliente',[ClienteController::class, 'index'])->name('cliente.index');
+        Route::get('/home/cliente/insertar',[ClienteController::class, 'insertar'])->name('cliente.insertar');
+        Route::get('/home/cliente/{Idcliente}',[ClienteController::class, 'show'])->name('cliente.show');
+        Route::get('/home/cliente/{Idcliente}/edit',[ClienteController::class, 'edit'])->name('cliente.edit');
 
-Route::post('/home/cliente',[MessageClient::class, 'store'])->name('cliente.store');//Messageclient se encargara de la gestion de clientes
-Route::post('/home/cliente/{Idcliente}/edit',[MessageClient::class, 'update'])->name('cliente.update');
+        Route::post('/home/cliente',[MessageClient::class, 'store'])->name('cliente.store');//Messageclient se encargara de la gestion de clientes
+        Route::post('/home/cliente/{Idcliente}/edit',[MessageClient::class, 'update'])->name('cliente.update');
 
-Route::get('/home/factura',[FacturaController::class, 'index'])->name('factura.index');
-Route::get('/home/factura/insertar',[FacturaController::class, 'insertar'])->name('factura.insertar');
+        Route::get('/home/factura',[FacturaController::class, 'index'])->name('factura.index');
+        Route::get('/home/factura/insertar',[FacturaController::class, 'insertar'])->name('factura.insertar');
 
-Route::post('/home/factura/insertar',[Messagefactura::class, 'store'])->name('factura.store');
+        Route::get('/home/factura/insertar/{id}/index',[FactDetalleController::class, 'index'])->name('factura.vistafactura');
+        Route::get('/home/factura/insertar/{id}/variante',[FactDetalleController::class, 'gettalla']);
+        Route::get('/home/factura/insertar/{id}/colores',[FactDetalleController::class, 'getcolor']);
+        Route::get('/home/factura/insertar/{id}/precio',[FactDetalleController::class, 'getprecio']);
+        Route::get('/home/factura/insertar/{id}/index/facturar',[FactDetalleController::class, 'facturador'])->name('factura.facturar');
+        Route::get('/home/factura/insertar/{id}/index/facturar/descargar',[FactDetalleController::class, 'descargar'])->name('factura.descargar');
 
-Route::get('/home/factura/insertar/{id}/index',[FactDetalleController::class, 'index'])->name('factura.vistafactura');
-Route::get('/home/factura/insertar/{id}/variante',[FactDetalleController::class, 'gettalla']);
-Route::get('/home/factura/insertar/{id}/colores',[FactDetalleController::class, 'getcolor']);
-Route::get('/home/factura/insertar/{id}/precio',[FactDetalleController::class, 'getprecio']);
+        Route::post('/home/factura/insertar',[Messagefactura::class, 'store'])->name('factura.store');
 
-Route::post('/home/factura/insertar/{id}/index',[FactDetalleController::class, 'store'])->name('factura.agregar');
+        Route::post('/home/factura/insertar/{id}/index',[FactDetalleController::class, 'store'])->name('factura.agregar');
+});
 
-Route::get('/home/factura/insertar/{id}/index/facturar',[FactDetalleController::class, 'facturador'])->name('factura.facturar');
-Route::get('/home/factura/insertar/{id}/index/facturar/descargar',[FactDetalleController::class, 'descargar'])->name('factura.descargar');
+
+
+
+
+
+
+
 
